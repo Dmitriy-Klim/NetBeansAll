81ded49be6271489cf40bc686e88fe9a38377c01
Dmitriy Sorokin <sorokindmv@gmail.com>
2016-05-30 16:46:07 +0300

    Добавление комментариев

--- BlinovStudyingJava/Blinov-Studying Java/src/blinov/studying/java/BlinovStudyingJava.java
+++ BlinovStudyingJava/Blinov-Studying Java/src/blinov/studying/java/BlinovStudyingJava.java
@@ -13,19 +13,46 @@ public class BlinovStudyingJava {
 
     /**
      * @param args the command line arguments
+     * Тут создается метод, который выводит на консоль строку.
      */
     public static void printcons() {
         System.out.println("Hello class world");
     }
+    /**
+     * Создание класса, который вовыодит строку на консоль.
+     */
     public static class printclass {
-        private void prn() {
+        public void prn() {
             System.out.println("Some thind to print on console");
+        }
+    }
+    public static class anyprint extends printclass {
+        private void prn2() {
+                System.out.println("String from extends class");
         }
     }
     public static void main(String[] args) {
         // TODO code application logic here
         printcons();
+     /**
+     * 
+     * Создание объекта класса.
+     */
         printclass p = new printclass();
+        anyprint p2 = new anyprint();
+     /**
+     * Обращение к методу объекта. 
+     * В обэекте p2 созданном от класса anyprint который наслудуется от класса 
+     * printclass 2 метода. Метод prn унаследованный и метод prn2 свой. 
+     * Метод prn можно унаследовать, т.к. от имеет статус public. Методы с 
+     * private не наследуются.
+     */
+        p2.prn2();
+        p2.prn();
+     /**
+     * 
+     * Обращение к методу объекта
+     */
         p.prn();
     }
     
