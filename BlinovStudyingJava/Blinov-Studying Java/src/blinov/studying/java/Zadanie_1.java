45a08c7a42fba0e36f132b80961019b6316af2e5
Dmitriy Sorokin <sorokindmv@gmail.com>
2016-05-31 15:26:29 +0300

    Задание 1. первые шаги

--- BlinovStudyingJava/Blinov-Studying Java/src/blinov/studying/java/Zadanie_1.java
+++ BlinovStudyingJava/Blinov-Studying Java/src/blinov/studying/java/Zadanie_1.java
@@ -10,6 +10,7 @@ package blinov.studying.java;
  * @author Admin
  */
 public class Zadanie_1 {
+    
 //    Класс, описывающий свойства объекта характерные для всех классов-наследников
     class Electropribory {
         String name;
@@ -20,6 +21,9 @@ public class Zadanie_1 {
     static class NagrevatelniePribory extends Electropribory {
         public NagrevatelniePribory (String name, int kilovaty, boolean rozetka, int litry) {
             System.out.println("Прибор "+ name+ " потребляет "+kilovaty+" ватт, на "+litry+" литров");
+            if (rozetka==true) {
+                System.out.println(name+" включен в розетку");    
+            }
             return ;
         }
     }
@@ -28,14 +32,22 @@ public class Zadanie_1 {
 
         public OsvetitelniePribory(String name, int kilovaty, boolean rozetka, int yarkost, int kollichastvolamp) {
            System.out.println("Прибор "+ name+ " потребляет "+kilovaty+" ватт. И отдает "+yarkost+" Люменов, при наличии "+kollichastvolamp+" ламп.");
-            return ; 
+           
+            if (rozetka==true) {
+            System.out.println(name+" включен в розетку");    
+            } 
+           return; 
         }
         
     }
     public static void main(String[] args) {
+        
+//        Создаем приборы
         NagrevatelniePribory boiler = new NagrevatelniePribory("Бойлер", 2000, true, 50);
         NagrevatelniePribory kipyatilnik = new NagrevatelniePribory("Кипятильник", 3000, false, 10);
         OsvetitelniePribory lustra = new OsvetitelniePribory("Люстра", 75, true, 13, 3);
         OsvetitelniePribory nastolnayalampa = new OsvetitelniePribory("Настольная лампа", 60, false, 8, 1);
+        
+        
     }
 }
