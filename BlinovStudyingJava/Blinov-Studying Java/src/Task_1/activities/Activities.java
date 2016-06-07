3f9a62c731866d88df5a1e2e864d0d90b03d7fd6
Dmitriy Sorokin <sorokindmv@gmail.com>
2016-06-07 14:47:19 +0300

    Готовое задание. До проверки.

--- BlinovStudyingJava/Blinov-Studying Java/src/Task_1/activities/Activities.java
+++ BlinovStudyingJava/Blinov-Studying Java/src/Task_1/activities/Activities.java
@@ -7,6 +7,7 @@ package Task_1.activities;
 
 import Task_1.Room.Room;
 import Task_1.Devices.ElectricalDevices;
+import java.util.Scanner;
 /**
  *
  * @author Admin
@@ -36,10 +37,33 @@ public class Activities {
         setConsumPower(m3Power);
     }
     
+    public void findDevice() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите мощьность прибора:  ");
+        int a = in.nextInt();
+        switch (a) {
+            case 2000: System.out.println("Это бойлер!");
+                        break;
+            case 3000: System.out.println("Это кипятильник!");
+                        break;
+            case 75: System.out.println("Это люстра!");
+                        break;
+            case 60: System.out.println("Это настольная лампа!");
+                        break;
+            default: System.out.println("Такого прибора нет. К сожалению. =(( ");
+           }
+        
+        
+        
+    }
+    
     
     public void result() {
     System.out.println("2. Общая мощьность всех электроприборов: "+getSummPower());
     System.out.println("3. Общая потредляемая мощьность: "+consumPower);
+    System.out.println("4. Поиск прибора в комнате по заданным параметрам");
+    findDevice();
     }
+    
     
 }
